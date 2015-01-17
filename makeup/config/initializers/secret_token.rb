ed5b4fd388f65c8ecce83645db7f5b62c35c4a31
ajk121 <contact.abdulkhan@gmail.com>
2015-01-17 15:44:07 +0000

    first commit

---
+++ makeup/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Makeup::Application.config.secret_token = '371434835080e38ea82238c585921a2aaa720e40cc7827cade47bab29e2f461d1faa47f290b28d2064c19ead09016ec96d247ea06e9be6cd4c6fe11e9c35e2fb'
